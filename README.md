f54dc76e50bd0c3d4168c21df3c5ff85d99c6e06
Fitzroy Woodruff <fitzroywoodruff@gmail.com>
2022-08-16 23:40:51 -0400

    Initial Commit
    This is the initial commit for the Three.js

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # This Is 3js
- This was my first attempt at using threejs for a project
+
+![Screenshot](img/ScreenShot.png)
+
+This was my first attempt at using threejs for a project. I am looking forward to importing other bodels and textures without the use of a CDN. The inition non-CDN version kept crashing and would load.
+
+## Outside CDNs and Sources
+
+- [Three.js CDN](https://cdnjs.cloudflare.com/ajax/libs/three.js/r99/three.min.js).
+- [Google Fonts](https://fonts.google.com/specimen/Montserrat?query=Montserrat).
+- [Pexels.com](https://images.pexels.com/photos/11635803/pexels-photo-11635803.jpeg).
---
+++ index.js
@@ -0,0 +1,45 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+	45,
+	window.innerWidth / window.innerHeight
+);
+camera.position.z = 3;
+camera.position.x = -1;
+scene.add(camera);
+
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load(
+	"https://images.pexels.com/photos/11635803/pexels-photo-11635803.jpeg"
+);
+
+const geo = new THREE.TorusKnotGeometry(0.5, 0.2, 100, 22);
+const mat = new THREE.MeshMatcapMaterial({ matcap: texture });
+const mesh = new THREE.Mesh(geo, mat);
+mesh.rotation.y = 0.5;
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.append(renderer.domElement);
+
+const cursor = { x: 0, y: 0 };
+window.addEventListener("mousemove", (_event) => {
+	cursor.x = _event.clientX / window.innerWidth - 0.5;
+	cursor.y = _event.clientY / window.innerHeight - 0.5;
+});
+
+const tick = () => {
+	window.requestAnimationFrame(tick);
+
+	mesh.rotation.y += 0.01;
+
+	const cameraX = cursor.x - 1;
+	const cameraY = -cursor.y;
+
+	camera.position.x += (cameraX - camera.position.x) / 5;
+	camera.position.y += (cameraY - camera.position.y) / 5;
+
+	renderer.render(scene, camera);
+};
+
+tick();
